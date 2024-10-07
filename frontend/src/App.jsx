a02e174d1e2a92140f59f8db1cbe9b17281df2f9
Ayan Banglawala <ayanchhipa2278@gmail.com>
2024-10-07 21:30:16 +0530

    second part completed designing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import Signup from "./pages/signup/Signup"
 
@@ -5,7 +6,7 @@ import Signup from "./pages/signup/Signup"
 function App() {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Signup/>
+      <Home/>
     </div>
   )
 }
---
+++ frontend/src/components/Sidebar/Logout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LuLogOut } from "react-icons/lu";
+
+const Logout = () => {
+  return (
+    <div className='mt-auto'>
+      <LuLogOut className='w-6 h-6 text-white cursor-pointer'/>
+    </div>
+  )
+}
+
+export default Logout
